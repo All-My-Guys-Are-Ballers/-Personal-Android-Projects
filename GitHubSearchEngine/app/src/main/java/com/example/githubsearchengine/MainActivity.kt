9836b1eaf8504b38ede0b235b132dc8b5547ffd4
Joshua Owolabi <joshuaoluwasegun99@gmail.com>
2022-11-22 23:42:12 +0100

    jhb

--- GitHubSearchEngine/app/src/main/java/com/example/githubsearchengine/MainActivity.kt
+++ GitHubSearchEngine/app/src/main/java/com/example/githubsearchengine/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.githubsearchengine
 
+import android.graphics.Paint.Align
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.githubsearchengine.ui.theme.GitHubSearchEngineTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    SearchBar()
                 }
             }
         }
@@ -33,6 +42,40 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
+
+@Preview
+@Composable
+fun SearchBar(){
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+
+    Row(modifier = Modifier.fillMaxWidth()){
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier.weight(1f),
+            label = {
+                Text(
+                    text = stringResource(id = R.string.search)
+                )
+            },
+            singleLine = true,
+            maxLines = 1,
+            shape = RoundedCornerShape(32.dp)
+        )
+        IconButton(onClick = { /*TODO*/ }, modifier = Modifier
+//            .align(Alignment.CenterVertically)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = "Search")
+        }
+    }
+}
+
+fun ListItem(){
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
---
+++ GitHubSearchEngine/app/src/main/java/com/example/githubsearchengine/data/Repository.kt
@@ -0,0 +1,7 @@
+package com.example.githubsearchengine.data
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Repository()
